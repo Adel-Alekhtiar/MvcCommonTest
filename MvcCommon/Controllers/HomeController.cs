21f22e79a81461b301d9f5ec906932d3e50a1ce1
Ziusking <fairthechoice@outlook.com>
2015-04-22 01:00:09 +0200

    test

---
+++ MvcCommon/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Mvc;
+
+namespace MvcCommon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "this is me testing";
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+            ViewBag.bob = "bob or whatever";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult mypage()
+        {
+           
+
+            return View();
+        }
+    }
+}
